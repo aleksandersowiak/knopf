89573b380ea5499d56c88a51e54ac4fd45648421
Aleksander Sowiak <aleksander.sowiak@codeconcept.pl>
2017-11-10 12:17:21 +0100

    pinImages

--- Models/Gallery.php
+++ Models/Gallery.php
@@ -9,20 +9,21 @@ class GalleryModel extends BaseModel
         }
         $data = $this->getLanguagesCase('category');
         $query = 'SELECT DISTINCT IF(ISNULL(category_'.$lang.'),
-CASE idx
-	'.$data['list'].'
-END
-, category_'.$lang.') AS category,
-g.id ,image, image_thumb, c.id as category_id,
-IF(ISNULL(category_'.$lang.'), 1,0) AS empty_category
-FROM gallery AS g
-JOIN (
-SELECT 1 AS idx
-'.$data['listSelect'].') t
-left join category as c on c.id = g.category_id
-'.$where.'
-HAVING category IS NOT NULL;
-';
+                    CASE idx
+                        '.$data['list'].'
+                    END
+                    , category_'.$lang.') AS category,
+                    g.id ,image, image_thumb, c.id as category_id,
+                    IF(ISNULL(category_'.$lang.'), 1,0) AS empty_category,
+                    g.product_id, g.realization
+                    FROM gallery AS g
+                    JOIN (
+                    SELECT 1 AS idx
+                    '.$data['listSelect'].') t
+                    left join category as c on c.id = g.category_id
+                    '.$where.'
+                    HAVING category IS NOT NULL;
+                    ';
         return $this->select($query);
     }
 
